34057b62bddb683258a5bd68d86e75d0ace71115
Danilo Flores <danilo040117@gmail.com>
2023-10-29 13:39:30 -0400

    add env

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}", "./App.js"],
+  theme: {
+    colors: {
+      primary: "#46B8C8",
+      secondary: "#FB8958",
+      background: "#FBFAFA",
+      white: "#FFFFFF",
+      black: "#3D4F5C",
+      orange: "#FFC351"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
